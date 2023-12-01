e78e5ee2e5dbf421a3d980424009811e264d077d
Oksimba <oksana.strus@eleks.com>
2023-12-01 16:58:31 +0200

    Add indexes for User and Card Entities

--- Solution/DataAccess/AccountServiceDbContext.cs
+++ Solution/DataAccess/AccountServiceDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess
 {
-    public class AccountServiceDbContext : DbContext
+    public class AccountServiceDbContext : DbContext, IDisposable
     {
         public AccountServiceDbContext(DbContextOptions<AccountServiceDbContext> options) : base(options)
         {
@@ -12,13 +12,31 @@ namespace DataAccess
         public DbSet<User> Users { get; set; }
         public DbSet<Card> Cards { get; set; }
 
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                base.Dispose();
+            }
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<User>().HasIndex(u => u.Login).IsUnique();
             CreateAccountCardsRelations(modelBuilder);
         }
 
         private void CreateAccountCardsRelations(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Card>()
+                .HasIndex(s => s.CardNumber).IsUnique();
+
             modelBuilder.Entity<Card>()
                 .HasOne(s => s.User)
                 .WithMany(s => s.Cards)
